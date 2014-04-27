e5abc7d4343d145eff1de2712e459582d32ab557
Theophilus Onime <tonime@andrew.cmu.edu>
2014-04-27 15:04:39 -0400

    bundle install with new gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery #with: :exception
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in?
+    current_user
+  end
+  helper_method :logged_in?
+
+  def check_login
+    redirect_to login_url, alert: "You need to log in to view this page." if current_user.nil?
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Go away or I shall taunt you a second time."
+    redirect_to home_path
+  end
 end
