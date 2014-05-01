5922cd1dcb1194d882e708c2365c967a9ae3a039
Theophilus Onime <tonime@andrew.cmu.edu>
2014-05-01 16:42:29 -0400

    doing nested forms

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,13 +1,14 @@
 class Instructor < ActiveRecord::Base
   include ChessCampHelpers
 
-  accepts_nested_attributes_for :users, reject_if: lambda { |user| user[:username].blank? }
+
   mount_uploader :picture, PictureUploader
 
   # relationships
   has_many :camp_instructors
   has_many :camps, through: :camp_instructors
   has_one :user
+  accepts_nested_attributes_for :user, reject_if: lambda { |user| user[:username].blank? }
 
   # validations
   validates_presence_of :first_name, :last_name, :phone
