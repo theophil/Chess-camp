effc43fac0daacd4ae2e9560e01f6c1db6190577
Theophilus Onime <tonime@andrew.cmu.edu>
2014-05-07 00:23:48 -0400

    basic but yucky admin dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
   	 @upcoming_camps_for_guest = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
-  	 @instructor_upcoming_camps = current_user.instructor.camps.upcoming.to_a if current_user.role == 'instructor' #current user WILL be instructor in this case
+  	 @instructor_upcoming_camps = current_user.instructor.camps.upcoming.to_a if logged_in? && current_user.role == 'instructor' #current user WILL be instructor in this case
+     # @instructor_students = current_user.instructor.students.to_a if current_user.role == 'instructor'
+     @camps = Camp.active.chronological.paginate(:page => params[:page]).per_page(10)
+     @instructors = Instructor.active.alphabetical.paginate(:page => params[:page]).per_page(10)
   end
 
   def about
