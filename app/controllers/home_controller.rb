70a8a4ba1d5ba33fc3af7f46e1cf296fc6752717
Theophilus Onime <tonime@andrew.cmu.edu>
2014-05-06 23:43:17 -0400

    instructor dashboard up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	 @upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
+  	 @upcoming_camps_for_guest = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
+  	 @instructor_upcoming_camps = current_user.instructor.camps.upcoming.to_a if current_user.role == 'instructor' #current user WILL be instructor in this case
   end
 
   def about
